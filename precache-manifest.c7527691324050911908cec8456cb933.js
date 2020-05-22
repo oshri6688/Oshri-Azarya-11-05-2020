5602b0fe64f02923a92fe03165cd8d0d6d703937
Oshri Azarya <OshriA@radware.com>
2020-05-22 23:30:23 +0300

    Updates

---
+++ precache-manifest.c7527691324050911908cec8456cb933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "394c3b0e607c99ef89b53d606d4f1cb1",
+    "url": "/Oshri-Azarya-11-05-2020/index.html"
+  },
+  {
+    "revision": "0d232ff32cd58619ae4f",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/2.746935d2.chunk.js"
+  },
+  {
+    "revision": "35a08d2e06945b779fadacb826071be3",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/2.746935d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c9e731d20e79427a396",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/main.2bc5264c.chunk.js"
+  },
+  {
+    "revision": "56b3c6ce8e1e30540ed6",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/runtime-main.442e0b16.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c5d521163da8ee6b16336011b7326e05.js"
+  "/Oshri-Azarya-11-05-2020/precache-manifest.c7527691324050911908cec8456cb933.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Oshri-Azarya-11-05-2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
