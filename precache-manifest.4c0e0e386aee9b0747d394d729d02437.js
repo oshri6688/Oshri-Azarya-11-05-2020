dff6e41c40a131831fe57d5f0d76a361ae4d8323
Oshri Azarya <OshriA@radware.com>
2020-05-22 22:34:49 +0300

    Updates

---
+++ precache-manifest.4c0e0e386aee9b0747d394d729d02437.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f982b41ddeabcf5fa139b37fceb9425c",
+    "url": "/Oshri-Azarya-11-05-2020/index.html"
+  },
+  {
+    "revision": "0d232ff32cd58619ae4f",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/2.746935d2.chunk.js"
+  },
+  {
+    "revision": "35a08d2e06945b779fadacb826071be3",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/2.746935d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006292e05d4af8766192",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/main.0bfbd1a1.chunk.js"
+  },
+  {
+    "revision": "56b3c6ce8e1e30540ed6",
+    "url": "/Oshri-Azarya-11-05-2020/static/js/runtime-main.442e0b16.js"
+  }
+]);
